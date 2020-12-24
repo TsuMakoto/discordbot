0d7d1cf85c1d40faf6dd68b0c27d22801b5333b9
TsuMakoto <tsukamoto@fusic.co.jp>
2020-12-24 19:32:15 +0900

    first commit

---
+++ CodeBlockExecuter.jl
@@ -0,0 +1,39 @@
+module CodeBlockExecuter
+
+using Discord
+
+
+# Create a handler for the MessageCreate event.
+function handler(c::Client, e::MessageCreate)
+  body = e.message.content
+
+  if !ismissing(e.message.author.bot)
+    return
+  end
+
+  regex = r"(?s)(?<=run\n`{3}julia)(.+)(?=`{3})"
+  m = match(regex, body)
+
+  if isnothing(m)
+    println("no match")
+  else
+    cmd = Cmd(["julia", "-e", string(m[1])])
+    parseresult = read(cmd, String)
+
+    # Add a reaction to the message.
+    reply(c, e.message, parseresult)
+  end
+end
+
+function main()
+  # Create a client.
+  c = Client(ENV["ACCESS_TOKEN"])
+  # Add the handler.
+  add_handler!(c, MessageCreate, handler)
+  # Log in to the Discord gateway.
+  open(c)
+  # Wait for the client to disconnect.
+  wait(c)
+end
+
+end; CodeBlockResponse.main()
